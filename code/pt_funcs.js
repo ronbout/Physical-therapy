cd7cb946d13d44b1bc4eb6e655c23ac1ea4742ab
Ronbout <ronbout@yahoo.com>
2024-07-15 17:32:12 -0500

    update google map code

--- code/pt_funcs.js
+++ code/pt_funcs.js
@@ -4,34 +4,29 @@
 
 var map;
 function initialize() {
-	var pt_loc = new google.maps.LatLng(35.492975, -97.293521);
-	var myOptions = {
-	  zoom: 15,
-	  center: pt_loc,
-	  mapTypeId: google.maps.MapTypeId.ROADMAP,
-	  disableDefaultUI: true
-	};
-	map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-	var markerOptions = {
-		position: pt_loc,
-		map: map
-	};
-	pt_map = new google.maps.Marker(markerOptions);
+  var pt_loc = new google.maps.LatLng(35.492975, -97.293521);
+  var myOptions = {
+    zoom: 15,
+    center: pt_loc,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDefaultUI: true,
+  };
+  // map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+  // var markerOptions = {
+  // 	position: pt_loc,
+  // 	map: map
+  // };
+  // pt_map = new google.maps.Marker(markerOptions);
 }
-function validate_form(thisButton)
-{
-	var thisForm = thisButton.form;
-	return true;
-}
-
-function disableEnterKey(e)
-{
-     var key;      
-     if(window.event)
-          key = window.event.keyCode; //IE
-     else
-          key = e.which; //firefox      
-
-     return (key != 13);
+function validate_form(thisButton) {
+  var thisForm = thisButton.form;
+  return true;
 }
 
+function disableEnterKey(e) {
+  var key;
+  if (window.event) key = window.event.keyCode; //IE
+  else key = e.which; //firefox
+
+  return key != 13;
+}
--- html/pt_map_right.php
+++ html/pt_map_right.php
@@ -15,12 +15,9 @@
 			</table></p>
 			<?php echo $pt_email;?></h4>
 			<br>
-			<div id="map_canvas"></div> 
-			<p class="center">
-				<a target="_blank"
-					href="http://maps.google.com/maps?q=13390+NE+23rd+St++Choctaw+OK+73020&hl=en&ll=35.495233,-97.29342&spn=0.006429,0.013475&sll=35.493325,-97.295378&sspn=0.001607,0.003369&vpsrc=6&hnear=13390+NE+23rd+St,+Choctaw,+Oklahoma+73020&t=m&z=16&iwloc=r0">
-				Click For Larger Map</a>
-			</p>
+			<div id="map_canvas">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6497.027317860777!2d-97.29682528496558!3d35.49157505147819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87b23aeeb5b8980f%3A0xd5059b54a7fc2f1e!2s13390%20NE%2023rd%20St%2C%20Choctaw%2C%20OK%2073020!5e0!3m2!1sen!2sus!4v1721082414115!5m2!1sen!2sus" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div> 
 			<h4>Hours:</h4>
 			<p><?php echo $pt_hours;?></p>
 			<?php require("html/pt_fb_right.php"); ?>
